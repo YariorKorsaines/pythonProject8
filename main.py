100068311f71552b04d2e925edc22e7b25aa92d4
Ярослав Корсика <yaroslav-korsika@mail.ru>
2022-09-14 21:29:42 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+from application.salary import calculate_salary
+from application.db.people import get_employees
+from datetime import date
+import sony_bravia_api
+if __name__ == '__main__':
+    calculate_salary(), get_employees()
+
+current_date = date.today()
+print(current_date)
